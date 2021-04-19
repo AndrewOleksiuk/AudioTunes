dad39d069d4ce16f7f09463ec436035311014945
Maksym Lytvynchuk <lytvynchuk.official@gmail.com>
2021-04-20 00:11:54 +0300

    add views for entities and interaction with them

---
+++ app/src/main/java/edu/lnu/audiotunes/presentation/fragments/adapters/artwork/holders/OnItemClickedListener.java
@@ -0,0 +1,7 @@
+package edu.lnu.audiotunes.presentation.fragments.adapters.artwork.holders;
+
+import android.view.View;
+
+public interface OnItemClickedListener {
+    void onClick(View view, int position);
+}
---
+++ app/src/main/java/edu/lnu/audiotunes/presentation/fragments/adapters/artwork/holders/StarButtonListener.java
@@ -0,0 +1,6 @@
+package edu.lnu.audiotunes.presentation.fragments.adapters.artwork.holders;
+
+public interface StarButtonListener {
+    void onLike(int position);
+    void onUnlike(int position);
+}
